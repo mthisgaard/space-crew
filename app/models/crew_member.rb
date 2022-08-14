2c8b4f516eb4f4cd66310fde6b09901fc09b5db1
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-14 18:27:12 +0900

    add dependecy on crew-members

--- app/models/crew_member.rb
+++ app/models/crew_member.rb
@@ -1,5 +1,5 @@
 class CrewMember < ApplicationRecord
-  has_many :orders
+  has_many :orders, dependent: :delete_all
   has_many :missions, through: :orders
   validates :name, presence: true
   validates :position, presence: true
