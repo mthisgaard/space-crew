1004e294ad1aca99e323a2bb4ef73b808f801a99
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-08-15 18:33:17 +0900

    make order comment optional add ternery operator to display

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :crew_member
   belongs_to :mission
-  validates :comment, presence: true
   validates :mission_id, uniqueness: { scope: :crew_member_id }
 end
